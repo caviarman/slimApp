60448a2c0e21e5126be647e85ae2762ebd1958eb
Vladimir Ilin <mark09072010@gmail.com>
2018-10-12 22:35:49 +0300

    new variant

--- public/index.php
+++ public/index.php
@@ -16,42 +16,95 @@ $repo = new Repository();
 $app = new \Slim\App($config);
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
-
-
+$container['flash'] = function () {
+    return new \Slim\Flash\Messages();
+};
+//new
 $app->get('/', function ($request, $response) {
     return $this->renderer->render($response, 'index.phtml');
 });
-
+//new
 $app->get('/users', function ($request, $response) use ($repo) {
+    $flash = $this->flash->getMessages();
+
     $params = [
+        'flash' => $flash,
         'users' => $repo->all()
     ];
-    return $this->renderer->render($response, 'users/show.phtml', $params);
-});
+    return $this->renderer->render($response, 'users/index.phtml', $params);
+})->setName('posts');
 
-$app->get('/users/new', function ($request, $response) {
-    return $this->renderer->render($response, 'users/new.phtml');
+//new 
+$app->get('/users/new', function ($request, $response) use ($repo) {
+    $params = [
+        'userData' => [],
+        'errors' => []
+    ];
+    return $this->renderer->render($response, 'users/new.phtml', $params);
 });
 
 $app->get('/users/[{id}]', function ($request, $response, $args) {
     $params = ['id' => $args['id']];
     return $this->renderer->render($response, 'users/edit.phtml', $params);
 });
-
+//new
 $app->post('/users', function ($request, $response) use ($repo) {
-    $user = $request->getParsedBodyParam('user');
-    $repo->save($user);
-    return $response->withRedirect('/users');
-});
+    $userData = $request->getParsedBodyParam('user');
 
-$app->get('/user/{id}/edit', function ($request, $response, array $args) use ($repo) {
-    $id = $args['id'];
-    $user = $repo->find($id);
+    $validator = new Validator();
+    $errors = $validator->validate($userData);
+
+    if (count($errors) === 0) {
+        $repo->save($userData);
+        $this->flash->addMessage('success', 'User has been created');
+        return $response->withRedirect($this->router->pathFor('users'));
+    }
 
     $params = [
-        'user' => $user
+        'userData' => $userData,
+        'errors' => $errors
     ];
 
-    return $this->renderer->render($response, "users/edit.phtml", $params);
+    return $this->renderer->render($response, 'users/new.phtml', $params);
+});
+//new
+$app->get('/users/{id}/edit', function ($request, $response, array $args) use ($repo) {
+    $user = $repo->find($args['id']);
+    $params = [
+        'user' => $user,
+        'userData' => $user
+    ];
+    return $this->renderer->render($response, 'users/edit.phtml', $params);
+});
+//new
+$app->patch('/users/{id}', function ($request, $response, array $args) use ($repo) {
+    $user = $repo->find($args['id']);
+    $userData = $request->getParsedBodyParam('user');
+
+    $validator = new Validator();
+    $errors = $validator->validate($userData);
+
+    if (count($errors) === 0) {
+        $user['name'] = $userData['name'];
+        $user['email'] = $userData['email'];
+        $user['city'] = $userData['city'];
+        $user['password'] = $userData['password'];
+        $repo->save($user);
+        $this->flash->addMessage('success', 'User has been updated');
+        return $response->withRedirect($this->router->pathFor('users'));
+    }
+    $params = [
+        'user' => $user,
+        'userData' => $userData,
+        'errors' => $errors
+    ];
+
+    return $this->renderer->render($response, 'users/edit.phtml', $params);
+});
+//new
+$app->delete('/users/{id}', function ($request, $response, array $args) use ($repo) {
+    $repo->destroy($args['id']);
+    $this->flash->addMessage('success', 'User has been deleted');
+    return $response->withRedirect($this->router->pathFor('users'));
 });
 $app->run();
